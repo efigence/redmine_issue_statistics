fdccd2bd746b6567cca814eab1945902ceaabd4e
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2014-08-05 14:57:09 +0200

    added permission

--- init.rb
+++ init.rb
@@ -10,7 +10,7 @@ Redmine::Plugin.register :redmine_issue_statistics do
   url 'https://github.com/efigence/redmine_issue_statistics'
   author_url 'http://www.efigence.com/'
 
-  permission :view_statistics, :issue_statistics => :index
+  permission :view_statistics, :issue_statistics => [:index, :users_stats, :projects_stats]
   menu :admin_menu, :issue_statistics, {:controller => 'issue_statistics', :action => 'index'}, :caption => 'Statistics'
 
 end
