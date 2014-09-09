b22fda34464b395ee6a24e54f5ad1536007ff368
swiatkiewicz <m.swiatkiewicz91@gmail.com>
2014-09-09 13:40:24 +0200

    change path for statistics

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 
-get "issue_statistics/index",  to: 'issue_statistics#index', as: 'statistics'
+get "issue_statistics/index",  to: 'issue_statistics#index', as: 'statistics', path: 'statistics'
 get "issue_statistics/users_stats",  to: 'issue_statistics#users_stats', as: 'statistics/users'
 get "issue_statistics/projects_stats",  to: 'issue_statistics#projects_stats', as: 'statistics/projects'
 get "issue_statistics/principal_stats_per_project",  to: 'issue_statistics#principal_stats_per_project', as: 'statistics/projects_principals'
